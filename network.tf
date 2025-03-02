70b22d68eb24d54c55b7c035381c0644d0fa2c96
ROMAN <rkozyrev730@gmail.com>
2025-03-02 14:08:47 +0300

    Created a Webservers group

--- network.tf
+++ network.tf
@@ -12,3 +12,10 @@ resource "yandex_vpc_subnet" "net-1" {
   network_id     = yandex_vpc_network.net-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
+
+resource "yandex_vpc_subnet" "net-2" {
+  name           = "lan20"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.net-1.id
+  v4_cidr_blocks = ["192.168.20.0/24"]
+}
